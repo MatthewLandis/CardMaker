a345eaaf56d73c85c1fefc31d5f93a3350310759
Your Name <you@example.com>
2026-01-11 21:24:36 -0500

    dssa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import domtoimage from 'dom-to-image';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('CardMaker');
+  atk = 2500;
+  def = 2000;
+  title = 'Dark Magician';
+  primaryType = 'Spellcaster';
+  coreType = '';
+  abilityType = '';
+  lastType = 'Normal';
+  effectText = "The ultimate wizard in terms of attack and defense.";
+
+  createCard() {
+    domtoimage.toJpeg(document.getElementById('card')!)
+      .then( (dataUrl) => {
+          var link = document.createElement('a');
+          link.download = this.title;
+          link.href = dataUrl;
+          link.click();
+      });
+  }
 }

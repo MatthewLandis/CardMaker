17636ecc57229d679ca6b662d1bb27cd2ccee4bb
Your Name <you@example.com>
2026-01-15 03:12:10 -0500

    Template dropdown

--- src/app/app.ts
+++ src/app/app.ts
@@ -21,28 +21,41 @@ export class App {
   lastType = 'Normal';
   effectText = "The ultimate wizard in terms of attack and defense.";
   attributeDropdownVisible = false;
+  templateDropdownVisible = false;
   attribute = 'Dark';
-
+  template = 'Normal';
   hoverLevel = 0;
 
   attributes = ['Dark', 'Light', 'Earth', 'Water', 'Fire', 'Wind', 'Divine'];
+  templates = ['Normal', 'Effect', 'Fusion', 'Ritual', 'Synchro', 'Xyz', 'Pendulum', 'Link', 
+    'Token', 'Slifer', 'Obelisk', 'Ra', 'LegendaryDragon', 'DarkSynchro', 'Skill', 'Spell', 'Trap'];
 
   createCard() {
     domtoimage.toJpeg(document.getElementById('card')!)
-      .then( (dataUrl) => {
-          var link = document.createElement('a');
-          link.download = this.title;
-          link.href = dataUrl;
-          link.click();
+      .then((dataUrl) => {
+        var link = document.createElement('a');
+        link.download = this.title;
+        link.href = dataUrl;
+        link.click();
       });
-  }  
+  }
 
-  toggleAttributeDropdown(){
+  toggleAttributeDropdown() {
     this.attributeDropdownVisible = !this.attributeDropdownVisible;
   }
+
+  toggleTemplateDropdown() {
+    this.templateDropdownVisible = !this.templateDropdownVisible;
+  }
+
 
   selectAttribute(attr: string) {
     this.attribute = attr;
     this.attributeDropdownVisible = false;
-}
+  }
+
+  selectTemplate(template: string) {
+    this.template = template;
+    this.templateDropdownVisible = false;
+  }
 }

1ab402f26a6f39dde2da25189927967a8e1ed4a5
Your Name <you@example.com>
2026-01-14 20:04:46 -0500

    LevelUpdated

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,12 +13,17 @@ import domtoimage from 'dom-to-image';
 export class App {
   atk = 2500;
   def = 2000;
+  level = 7;
   title = 'Dark Magician';
   primaryType = 'Spellcaster';
   coreType = '';
   abilityType = '';
   lastType = 'Normal';
   effectText = "The ultimate wizard in terms of attack and defense.";
+  attributeDropdownVisible = false;
+  attribute = 'Dark';
+
+  hoverLevel = 0;
 
   createCard() {
     domtoimage.toJpeg(document.getElementById('card')!)
@@ -28,5 +33,14 @@ export class App {
           link.href = dataUrl;
           link.click();
       });
+  }  
+
+  a(){
+    this.attributeDropdownVisible = !this.attributeDropdownVisible;
   }
+
+  selectAttribute(attr: string) {
+    this.attribute = attr;
+    this.attributeDropdownVisible = false;
+}
 }

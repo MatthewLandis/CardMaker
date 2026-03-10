c65b24dc8c951de07bc0eaec3dcf01cef399d0b4
Your Name <you@example.com>
2026-03-10 18:32:24 -0400

    back slash shulk

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cardmaker-backend-production.up.railway.app/',
+  apiUrl: 'https://cardmaker-backend-production.up.railway.app',
 };

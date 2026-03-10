e769beba804d68ba2a8b741d91c559d24f889af3
Your Name <you@example.com>
2026-03-10 18:38:13 -0400

    yin slash

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import { routes } from './routes/route.js';
 
 const app = express();
 
-const clientHost = process.env['ENV']! === 'production' ? 'https://cardmaker-frontend-production.up.railway.app/' : 'http://localhost:4200';
+const clientHost = process.env['ENV']! === 'production' ? 'https://cardmaker-frontend-production.up.railway.app' : 'http://localhost:4200';
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true })); //fixes "PayloadTooLargeError: request entity too large" error when sending large images
